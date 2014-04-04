8a18776d742d5d390802c0306d4e1bb871260ca9
Andrew Chapman <andy@simb.com.au>
2014-04-04 23:03:58 +1100

    Preparing phonegap build

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ function App()
 	// this.apiURL = "http://projects.loc/blueprintapi/";
     //this.apiURL = "http://192.168.56.101/blueprint/api/";
 	this.phonegapBuild = true; 	// Set this to true when phonegap is the target
-	this.version = 15;				// Identifies the app version to the server
+	this.version = 16;				// Identifies the app version to the server
 	this.versionStatus = "Production";
 	this.localMode = false;
 	this.context = "";
