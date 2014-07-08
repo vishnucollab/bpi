ca0d8f2d7c4c507858443a1313209e4afdf5c772
Andrew Chapman <andy@simb.com.au>
2014-07-08 23:44:28 +1000

    Preparing for deployment

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /***
 * @author: Andrew Chapman
-* @copyright: SIMB Pty Ltd 2010 - 2011
+* @copyright: SIMB Pty Ltd 2010 - 2014
 * 
 * Need to show defect level notes
 * Need to show more than 3 images on a report
@@ -16,7 +16,7 @@ function App()
 	//this.apiURL = "http://projects.loc/blueprintapi/";
     //this.apiURL = "http://192.168.1.52/blueprint/api/";
 	this.phonegapBuild = false; 	// Set this to true when phonegap is the target
-	this.version = 1.11;				// Identifies the app version to the server
+	this.version = 1.12;				// Identifies the app version to the server
 	this.versionStatus = "Production";
 	this.localMode = false;
 	this.context = "";
