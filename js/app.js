0848b0831e7fdd1d549a5aa02a7c4e27f1144292
Andrew Chapman <andy@simb.com.au>
2015-03-26 15:11:50 +1100

    Switching back to dev

--- js/app.js
+++ js/app.js
@@ -10,12 +10,12 @@ function App()
 {
 	var self = this;	// Create a reference to the object itself
 	//this.apiURL = "http://localhost/nycran/blueprintapi/";
-	//this.apiURL = "http://qa.simb.com.au/blueprint/api/";  
-    this.apiURL = "http://blueprint.simb.com.au/api/";
+	this.apiURL = "http://qa.simb.com.au/blueprint/api/";  
+    //this.apiURL = "http://blueprint.simb.com.au/api/";
 	//this.apiURL = "http://blue.print/BlueprintAPI/";
 	//this.apiURL = "http://projects.loc/blueprintapi/";
     //this.apiURL = "http://192.168.1.52/blueprint/api/";
-	this.phonegapBuild = true; 	// Set this to true when phonegap is the target
+	this.phonegapBuild = false; 	// Set this to true when phonegap is the target
 	this.version = 1.24;				// Identifies the app version to the server
 	this.versionStatus = "Production";
 	this.localMode = false;
