1bd84eff9949116072f09373915c23a585093c18
Dev 3 <dev3@simb.com.au>
2017-12-11 13:00:27 +0700

    2.0.11

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ function App()
 	//this.apiURL = "http://projects.loc/blueprintapi/";
     //this.apiURL = "http://192.168.0.52/blueprint/api/";
 	this.phonegapBuild = true; 	// Set this to true when phonegap is the target
-	this.version = '2.0.10';				// Identifies the app version to the server
+	this.version = '2.0.11';				// Identifies the app version to the server
 	this.versionStatus = "Production";
 	this.localMode = false;
 	this.context = "";
