7190e4417c7802682067ca08e56e328c4ad42029
Dev 3 <dev3@simb.com.au>
2018-02-09 22:21:26 +0700

    16870 - Practical Completion option

--- js/inspections.js
+++ js/inspections.js
@@ -3210,6 +3210,7 @@ var Inspections = function()
 
         $('#practical_completed_selector').change(function(){
             $('#practical_completed').val($(this).val());
+            self.checkSaveInspection(0);
         });
 	}
 
