5ab9136ac6b362f40272835e7351b84725506fe2
nthieu <hieu.ps.nguyen@gmail.com>
2020-07-08 22:53:09 +0700

    [Hotfix] Debug

--- js/inspections.js
+++ js/inspections.js
@@ -7600,6 +7600,15 @@ var Inspections = function()
                     "AND r.id = ? " +
                     "ORDER BY ii.seq_no, ii.seq_no2 ASC";
 
+                var sql = "SELECT ri.id, ii.seq_no, ii.location, ii.question, ii.action, ii.observation, ri.rectified, r.failed " +
+                    "FROM reinspectionitems ri " +
+                    "LEFT JOIN significant_items si ON si.foreign_id = ri.id AND si.deleted != 1 AND r.id = ? " +
+                    "INNER JOIN inspectionitems ii ON ri.inspectionitem_id = ii.id " +
+                    "INNER JOIN reinspections r ON r.id = ri.reinspection_id " +
+                    "WHERE ii.deleted = 0 " +
+                    "AND r.id = ? " +
+                    "ORDER BY ii.seq_no, ii.seq_no2 ASC";
+
                 $("#reinspectionScrollWrapper").html("");
                 objApp.showHideSpinner(true, "#reinspection");
                 objDBUtils.loadRecordsSQL(sql, [reinspection_id, reinspection_id], function(param, items) {
